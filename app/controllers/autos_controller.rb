268152de070e1a59f919a79d4d95846b3b39b379
pipehs <pipe.hs@gmail.com>
2015-08-18 03:45:00 -0400

    Agregado CREATE de autos

--- app/controllers/autos_controller.rb
+++ app/controllers/autos_controller.rb
@@ -7,4 +7,18 @@ class AutosController < ApplicationController
   	@auto = Auto.find(params[:id])
   end
 
+  def new
+  	@auto = Auto.new
+  end
+
+  def create
+  	@auto = Auto.new(marca: params[:auto][:marca],
+  					 modelo: params[:auto][:modelo])
+  	if @auto.save
+  		redirect_to @auto
+  	else
+  		render :new
+  	end
+  end
+
 end
--- app/models/auto.rb
+++ app/models/auto.rb
@@ -1,3 +1,5 @@
 class Auto < ActiveRecord::Base
 	has_many :partes
+	validates :marca, presence: true
+	validates :modelo, presence: true
 end
